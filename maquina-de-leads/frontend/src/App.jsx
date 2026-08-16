2e5d2c3d20865db1ccf4e564aa862df607ff59d1
acastilho <andxuxa@gmail.com>
2026-08-16 09:41:12 -0300

    feat: route campaign workspace

--- maquina-de-leads/frontend/src/App.jsx
+++ maquina-de-leads/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import Dashboard from './pages/Dashboard.jsx';
+import CampaignDetail from './pages/CampaignDetail.jsx';
 import NicheDetail from './pages/NicheDetail.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 
@@ -10,22 +11,9 @@ export default function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/nichos/:id"
-        element={
-          <PrivateRoute>
-            <NicheDetail />
-          </PrivateRoute>
-        }
-      />
+      <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      <Route path="/campanhas/:id" element={<PrivateRoute><CampaignDetail /></PrivateRoute>} />
+      <Route path="/nichos/:id" element={<PrivateRoute><NicheDetail /></PrivateRoute>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
