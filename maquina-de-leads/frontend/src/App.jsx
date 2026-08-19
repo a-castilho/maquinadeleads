9dd77dfdd5215138e5be763b75a5e422d7bd1387
acastilho <andxuxa@gmail.com>
2026-08-18 22:47:33 -0300

    refactor: wrap private screens in Ubuntu shell

--- maquina-de-leads/frontend/src/App.jsx
+++ maquina-de-leads/frontend/src/App.jsx
@@ -5,15 +5,24 @@ import Dashboard from './pages/Dashboard.jsx';
 import CampaignDetail from './pages/CampaignDetail.jsx';
 import NicheDetail from './pages/NicheDetail.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
+import UbuntuShell from './components/UbuntuShell.jsx';
+
+function ProtectedPage({ children }) {
+  return (
+    <PrivateRoute>
+      <UbuntuShell>{children}</UbuntuShell>
+    </PrivateRoute>
+  );
+}
 
 export default function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-      <Route path="/campanhas/:id" element={<PrivateRoute><CampaignDetail /></PrivateRoute>} />
-      <Route path="/nichos/:id" element={<PrivateRoute><NicheDetail /></PrivateRoute>} />
+      <Route path="/" element={<ProtectedPage><Dashboard /></ProtectedPage>} />
+      <Route path="/campanhas/:id" element={<ProtectedPage><CampaignDetail /></ProtectedPage>} />
+      <Route path="/nichos/:id" element={<ProtectedPage><NicheDetail /></ProtectedPage>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
